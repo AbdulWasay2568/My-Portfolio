3636206c62fa90f9bbe6e98bca92c7f4a8e17653
Abdul Wasay <abdulwasaydogar2002@gmail.com>
2025-01-15 22:03:37 +0500

    Style changes

--- src/components/SkillsList.tsx
+++ src/components/SkillsList.tsx
@@ -1,34 +1,35 @@
 export default function SkillsList(){
     const skills = {
         "Web Development": {
-            Frontend: ['HTML', 'HTML5', 'CSS', 'JavaScript', 'React.js', 'Next.js' , 'Tailwind CSS', 'TypeScript'],
+            Frontend: ['HTML', 'CSS', 'JavaScript', 'React.js', 'Next.js' , 'Tailwind CSS', 'TypeScript'],
             Backend: ['Node.js', 'Express.js', 'Prisma ORM'],
             Database: ['PostgreSQL', 'MySQL', 'MS SQL', 'Firebase', 'Supabase'],
-            Devops: ['Git']
+            // Devops: ['Git']
         },
         "App Development": {
             Frontend: ['React Native', 'TypeScript', 'NativeWind'],
             Backend: ['Node.js', 'Express.js', 'Prisma ORM'],
             Database: ['PostgreSQL', 'MySQL', 'MS SQL', 'Firebase', 'Supabase'],
-            Devops: ['Git']
+            // Devops: ['Git']
         },
     };
     
     return (
-        <div className="min-h-screen bg-[#121212] rounded-lg text-white p-4">
+        <div className="min-h-screen bg-black rounded-lg text-zinc-100 p-4">
+            <p className="text-xl font-bold">What I Know</p>
             {/* Main Section */}
                 {Object.entries(skills).map(([category, sections]) => (
-                    <div key={category} className= "mt-8">
+                    <div key={category} className= "mt-8 justify-center flex">
                         {/* Category Section */}
-                        <div className="bg-black p-4 rounded-lg">
+                        <div className="bg-gray-950 p-4 rounded-lg w-[850px]">
                             <b className="text-center block text-white">{category}</b>
-                            <div className="flex flex-wrap  justify-center mt-4 gap-4">
+                            <div className="flex flex-wrap justify-center mt-4 gap-4">
                                 {Object.entries(sections).map(([section, technologies]) => (
-                                    <div key={section} className="bg-[#121212] p-4 rounded-lg w-[350px] h-[170px]  lg:w-[190px] lg:h-[250px]">
+                                    <div key={section} className="bg-[#121212] p-4 rounded-lg w-[350px] h-[170px]  lg:w-[190px] lg:h-[250px] border border-gray-800 shadow-lg">
                                         <b>{section}</b>
                                         <div className="flex flex-wrap mt-2 justify-center gap-2">
                                             {technologies.map((tech) => (
-                                                <div key={tech} className="bg-gray-800 p-2 mt-2 rounded-lg w-auto text-xs">{tech}</div>
+                                                <div key={tech} className="bg-gray-800 border border-gray-700 shadow-lg p-2 mt-2 rounded-lg w-auto text-xs">{tech}</div>
                                             ))}
                                         </div>
                                     </div>
